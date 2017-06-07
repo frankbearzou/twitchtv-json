475630d33b01c37c9dbdb95997f9e211c71fd4df
Frank Zou <frankbearzou@gmail.com>
2017-06-06 21:45:03 -0400

    fix bugs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "tsm_viss", "tecnosh"];
   let base_url = "https://wind-bow.gomix.me/twitch-api/"; // + freecodecamp + "?callback=?";
 
   for (let i = 0; i < channels.length; i++) {
@@ -28,19 +28,22 @@ $(document).ready(function () {
       let channels_url = base_url + 'channels/' + channels[i] + '?callback=?';
       $.getJSON(channels_url, function (data) {
         logo = data.logo;
-        display_name = data.display_name;
+        display_name = data.display_name.length > 15 ? data.display_name.substr(0, 15) + "..." : data.display_name;
         url = data.url;
         if (status === 'online') {
           game = data.game + ':' + data.status;
+          console.log(data.game);
+          //game = data.game;
+
         } else {
           game = 'offline';
         }
 
       }).done(function (data) {
         let html = "<div class='row result-list " + status + "'>" +
-          "<div class='col-lg-2 col-sm-1'><img class='logo' src='" + logo + "'></img></div>" +
-          "<div class='col-lg-2 col-sm-1 display_name'><a href='" + url + "' target='_blank'>" + display_name + "</a></div>" +
-          "<div class='col-lg-8 col-sm-10 game'>" + game + "</div>" +
+          "<div class='col-md-2 col-sm-2 col-xs-3 center-block logo'><img class='img-circle img-responsive center-block' src='" + logo + "' height='48px' width='48px'></img></div>" +
+          "<div class='col-md-2 col-sm-2 col-xs-3 display_name'><a href='" + url + "' target='_blank'>" + display_name + "</a></div>" +
+          "<div class='col-md-8 col-sm-8 col-xs-6 game'>" + game + "</div>" +
           "</div>";
 
         $('.results').append(html);
